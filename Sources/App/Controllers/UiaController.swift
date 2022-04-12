79ad3e1ec55093a818b5ed9f04ec17bff653624b
Charles Wright <cvwright@futo.org>
2022-04-12 18:36:15 -0500

    Cleaning up the main controller in preparation for handling actual API endpoints

--- Sources/App/Controllers/UiaController.swift
+++ Sources/App/Controllers/UiaController.swift
@@ -55,7 +55,8 @@ struct UiaController: RouteCollection {
     
     func boot(routes: RoutesBuilder) throws {
         for route in self.config.routes {
-            let pathComponents = route.path.split(separator: "/").map { PathComponent(stringLiteral: String($0)) }
+            let pathComponents = route.path.split(separator: "/").map { PathComponent(stringLiteral: String($0))
+            }
             routes.on(route.method, pathComponents, use: { (req) -> Response in
                 
                 req.logger.debug("Top-level handler got a request for \(route.path)")
@@ -66,7 +67,7 @@ struct UiaController: RouteCollection {
                     req.logger.debug("Failed to decode request content")
                 }
                 
-                try await handleUIA(req: req)
+                try await handleUIA(req: req, flows: route.flows)
                 // Now figure out what to do
                 // * Is the route one of our own that we should handle internally?
                 // * Or is it one that we should proxy to the homeserver?
@@ -82,7 +83,7 @@ struct UiaController: RouteCollection {
         return String( (0 ..< length).map { _ in "0123456789".randomElement()! } )
     }
     
-    private func _getUserId(req: Request) -> String? {
+    private func _getUserId(req: Request) async throws -> String? {
         guard let bearerHeader = req.headers.bearerAuthorization else {
             return nil
         }
@@ -97,19 +98,7 @@ struct UiaController: RouteCollection {
     // FIXME Add a callback so that we can handle UIA and then do something else
     //       Like, sometimes we want to proxy the "real" request (sans UIA) to the homeserver
     //       But other times, we need to handle the request ourselves in another handler
-    func handleUIA(req: Request) async throws {
-
-        // First let's make sure that this is one of our configured routes,
-        // and let's get its configuration
-        guard let route = self.config.routes.first(where: {
-            $0.path == req.url.path && $0.method == req.method
-        }) else {
-            // We're not even supposed to be here
-            req.logger.error("UIA handler got a request for an un-handled route")
-            throw Abort(.internalServerError)
-        }
-
-        let flows = route.flows
+    func handleUIA(req: Request, flows: [UiaFlow]) async throws {
         
         let userId = try await _getUserId(req: req)
         
@@ -140,7 +129,7 @@ struct UiaController: RouteCollection {
 
         let authType = auth.type
         // Is this one of the auth types that are required here?
-        let allStages = flows.reduce( Set<String>()) { (curr,next) in
+        let allStages = flows.reduce( Set<String>() ) { (curr,next) in
             curr.union(Set(next.stages))
         }
         guard allStages.contains(authType) else {
@@ -182,11 +171,14 @@ struct UiaController: RouteCollection {
                 if completedStages.isSuperset(of: flowStages) {
                     // Yay we're done with UIA
                     // Let's get out of here -- Let the main handler do whatever it needs to do with the "real" request
-                    req.logger.debug("UIA controller: Yay we're done with UIA")
+                    req.logger.debug("UIA controller: Yay we're done with UIA.  Completed flow = \(flow.stages)")
                     return
                 }
             }
             
+            // We're still here, so we must not be done yet
+            // Therefore we have more UIA stages left to go
+            // Get their parameters for the UIA response
             var newParams: [String: [String: AnyCodable]] = [:]
             for flow in flows {
                 for stage in flow.stages {
