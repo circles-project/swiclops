8d4b29a34d34246c377a50ff3f2663adaa68a5eb
Charles Wright <cvwright@futo.org>
2022-04-21 13:06:42 -0500

    Split out the App Store receipt and in-app purchase into their own files

--- Sources/App/Models/AppStore+Receipt.swift
+++ Sources/App/Models/AppStore+Receipt.swift
@@ -1,8 +1,42 @@
 //
-//  File.swift
-//  
-//
+//  AppStore+Receipt.swift
+//   * Based on https://github.com/slashmo/swift-app-store-receipt-validation
+//   * Original author: Moritz Lang
+//   * License: Apache 2.0
 //  Created by Charles Wright on 4/21/22.
 //
 
-import Foundation
+import Vapor
+
+extension AppStore {
+    public struct Receipt: Content {
+        public let bundleId: String
+        public let applicationVersion: String
+        public let inApp: [InAppPurchase]
+        public let originalApplicationVersion: String
+        public let receiptCreationDate: Date
+        public let receiptExpirationDate: Date?
+
+        enum CodingKeys: String, CodingKey {
+            case bundleId = "bundle_id"
+            case applicationVersion = "application_version"
+            case inApp = "in_app"
+            case originalApplicationVersion = "original_application_version"
+            case receiptCreationDate = "receipt_creation_date_ms"
+            case receiptExpirationDate = "receipt_expiration_date_ms"
+        }
+    }
+}
+
+public extension AppStore.Receipt {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.bundleId = try container.decode(String.self, forKey: .bundleId)
+        self.applicationVersion = try container.decode(String.self, forKey: .applicationVersion)
+        self.inApp = try container.decode([AppStore.InAppPurchase].self, forKey: .inApp)
+        self.originalApplicationVersion = try container.decode(String.self, forKey: .originalApplicationVersion)
+        self.receiptCreationDate = try container.decodeAppStoreDate(forKey: .receiptCreationDate)
+        self.receiptExpirationDate = try container.decodeAppStoreDateIfPresent(forKey: .receiptExpirationDate)
+    }
+}
