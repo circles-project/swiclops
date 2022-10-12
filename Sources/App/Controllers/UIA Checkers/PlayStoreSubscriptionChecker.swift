28caa00df15c42052062b8c4aff50fb3a619df8e
Charles Wright <cvwright@futo.org>
2022-10-12 17:14:51 -0500

    Initial attempt at support for Google Play Store subscriptions

--- Sources/App/Controllers/UIA Checkers/PlayStoreSubscriptionChecker.swift
+++ Sources/App/Controllers/UIA Checkers/PlayStoreSubscriptionChecker.swift
@@ -12,7 +12,8 @@ import AnyCodable
 
 
 struct PlayStoreSubscriptionChecker: AuthChecker {
-    let AUTH_TYPE_PLAYSTORE_SUBSCRIPTION = "org.futo.subscription.google_play"
+    static let AUTH_TYPE_PLAYSTORE_SUBSCRIPTION = "org.futo.subscription.google_play"
+    
     var productIds: [String]
     
     init(productIds: [String]) {
@@ -20,7 +21,9 @@ struct PlayStoreSubscriptionChecker: AuthChecker {
     }
     
     func getSupportedAuthTypes() -> [String] {
-        [AUTH_TYPE_PLAYSTORE_SUBSCRIPTION]
+        [
+            PlayStoreSubscriptionChecker.AUTH_TYPE_PLAYSTORE_SUBSCRIPTION
+        ]
     }
     
     func getParams(req: Request, sessionId: String, authType: String, userId: String?) async throws -> [String : AnyCodable]? {
@@ -33,15 +36,19 @@ struct PlayStoreSubscriptionChecker: AuthChecker {
             struct AuthDict: UiaAuthDict {
                 var type: String
                 var session: String
-                var purchaseToken: String  // This one stays the same across every renewal of a given subscription
-                                           // Note from: https://developer.android.com/google/play/billing/index.html "Subscription upgrades, downgrades, and other subscription purchase flows generate purchase tokens that must replace a previous purchase token."
-                var orderId: String        // This one is unique for each purchase / renewal
+                
+                var package: String        // Which app made the purchase? In the future we might have multiple different apps, eg the social app plus a photo gallery plus a chat client etc etc etc
+                
+                var subscriptionId: String // The purchased subscription ID (for example, 'monthly001').
+                
+                var token: String          // The token provided to the user's device when the subscription was purchased.
                 
                 enum CodingKeys: String, CodingKey {
                     case type
                     case session
-                    case purchaseToken = "purchase_token"
-                    case orderId = "order_id"
+                    case package
+                    case subscriptionId = "subscription_id"
+                    case token = "token"
                 }
             }
             var auth: AuthDict
@@ -52,12 +59,65 @@ struct PlayStoreSubscriptionChecker: AuthChecker {
             req.logger.error("\(msg)") // The need for this dance is moronic.  Thanks, SwiftLog.
             throw MatrixError(status: .badRequest, errcode: .badJson, error: msg)
         }
+        let auth = uiaRequest.auth
+        let session = req.uia.connectSession(sessionId: auth.session)
+
+        let package = auth.package
+        let subscriptionId = auth.subscriptionId
+        let token = auth.token
         
-        throw Abort(.notImplemented)
+        // First order of business: Are these valid parameters for our available subscriptions?
+        if !productIds.contains(subscriptionId) {
+            throw MatrixError(status: .forbidden, errcode: .invalidParam, error: "Invalid subscription product id")
+        }
+        // FIXME: Also check package
+        
+        // Ok we have a valid request from the client
+        // Now we need to validate their subscription purchase
+        // See https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptions/get
+        let validationURL = URI(
+            string: "https://androidpublisher.googleapis.com/androidpublisher/v3/applications/\(package)/purchases/subscriptions/\(subscriptionId)/tokens/\(token)"
+        )
+        
+        // Send a GET request to the validation URL
+        let googleResponse = try await req.client.get(validationURL)
+        
+        // The response should contain transaction data
+        guard let transaction = try? googleResponse.content.decode(GooglePlay.TransactionData.self) else {
+            throw MatrixError(status: .unauthorized, errcode: .unauthorized, error: "Failed to validate subscription purchase")
+        }
+        
+        // Verify that the subscription period includes the current time
+        let now = Date()
+        let startDate = Date(timeIntervalSince1970: Double(transaction.startTimeMillis))
+        let endDate = Date(timeIntervalSince1970: Double(transaction.expiryTimeMillis))
+        let wiggleRoom: TimeInterval = 5 * 60.0  // Allow for clocks to be off by up to 5 minutes
+        
+        guard startDate.distance(to: now) > -wiggleRoom && now.distance(to: endDate) > 0 else {
+            throw MatrixError(status: .unauthorized, errcode: .invalidParam, error: "Subscription is not currently valid")
+        }
+        
+        // If the transaction needs to be acknowledged, then go ahead and send the acknowledgement now
+        // Otherwise Google will automatically cancel it after X hours
+        if transaction.acknowledgementState == 0 {
+            let acknowledgementURL = URI(string: "https://androidpublisher.googleapis.com/androidpublisher/v3/applications/\(package)/purchases/subscriptions/\(subscriptionId)/tokens/\(token):acknowledge")
+            let ackResponse = try await req.client.post(acknowledgementURL)
+            guard ackResponse.status == .ok else {
+                throw MatrixError(status: .internalServerError, errcode: .unknown, error: "Failed to acknowledge transaction")
+            }
+        }
+
+        // Ok everything seems to check out.  Looks like we're good to go.
+        // Store the subscription info in the UIA state so we can save it to the database in onEnroll()
+        await session.setData(for: PlayStoreSubscriptionChecker.AUTH_TYPE_PLAYSTORE_SUBSCRIPTION+".subscription_id", value: subscriptionId)
+        await session.setData(for: PlayStoreSubscriptionChecker.AUTH_TYPE_PLAYSTORE_SUBSCRIPTION+".token", value: token)
+        
+        // And we're good!
+        return true
     }
     
     func onLoggedIn(req: Request, userId: String) async throws {
-        throw Abort(.notImplemented)
+        // Do nothing
     }
     
     func onEnrolled(req: Request, authType: String, userId: String) async throws {
---
+++ Sources/App/Models/GooglePlay.swift
@@ -0,0 +1,175 @@
+//
+//  GooglePlay.swift
+//  
+//
+//  Created by Charles Wright on 10/12/22.
+//
+
+import Vapor
+
+enum GooglePlay {
+    
+    struct TransactionData: Content {
+        // kind
+        // string
+        // This kind represents a subscriptionPurchase object in the androidpublisher service.
+        var kind: String
+
+        // startTimeMillis
+        // string (int64 format)
+        // Time at which the subscription was granted, in milliseconds since the Epoch.
+        var startTimeMillis: Int64
+
+        // expiryTimeMillis
+        // string (int64 format)
+        // Time at which the subscription will expire, in milliseconds since the Epoch.
+        var expiryTimeMillis: Int64
+
+        // autoResumeTimeMillis
+        // string (int64 format)
+        // Time at which the subscription will be automatically resumed, in milliseconds since the Epoch. Only present if the user has requested to pause the subscription.
+        var autoResumeTimeMillis: Int64?
+
+        // autoRenewing
+        // boolean
+        // Whether the subscription will automatically be renewed when it reaches its current expiry time.
+        var autoRenewing: Bool
+        
+        // priceCurrencyCode
+        // string
+        // ISO 4217 currency code for the subscription price. For example, if the price is specified in British pounds sterling, priceCurrencyCode is "GBP".
+        var priceCurrencyCode: String
+
+        // priceAmountMicros
+        // string (int64 format)
+        // Price of the subscription, For tax exclusive countries, the price doesn't include tax. For tax inclusive countries, the price includes tax. Price is expressed in micro-units, where 1,000,000 micro-units represents one unit of the currency. For example, if the subscription price is €1.99, priceAmountMicros is 1990000.
+        var priceAmountMicros: Int64
+
+        // introductoryPriceInfo
+        // object (IntroductoryPriceInfo)
+        // Introductory price information of the subscription. This is only present when the subscription was purchased with an introductory price. This field does not indicate the subscription is currently in introductory price period.
+        var introductoryPriceInfo: IntroductoryPriceInfo
+        
+        // https://developers.google.com/android-publisher/api-ref/rest/v3/purchases.subscriptions#IntroductoryPriceInfo
+        struct IntroductoryPriceInfo: Codable {
+            // introductoryPriceCurrencyCode
+            // string
+            // ISO 4217 currency code for the introductory subscription price. For example, if the price is specified in British pounds sterling, priceCurrencyCode is "GBP".
+            var introductoryPriceCurrencyCode: String
+
+            // introductoryPriceAmountMicros
+            // string (int64 format)
+            // Introductory price of the subscription, not including tax. The currency is the same as priceCurrencyCode. Price is expressed in micro-units, where 1,000,000 micro-units represents one unit of the currency. For example, if the subscription price is €1.99, priceAmountMicros is 1990000.
+            var introductoryPriceAmountMicros: Int64
+
+            // introductoryPricePeriod
+            // string
+            // Introductory price period, specified in ISO 8601 format. Common values are (but not limited to) "P1W" (one week), "P1M" (one month), "P3M" (three months), "P6M" (six months), and "P1Y" (one year).
+            var introductoryPricePeriod: String
+
+            // introductoryPriceCycles
+            // integer
+            // The number of billing period to offer introductory pricing.
+            var introductoryPriceCycles: Int
+        }
+
+        // countryCode
+        // string
+        // ISO 3166-1 alpha-2 billing country/region code of the user at the time the subscription was granted.
+        var countryCode: String
+
+        // developerPayload
+        // string
+        // A developer-specified string that contains supplemental information about an order.
+        var developerPayload: String
+
+        // paymentState
+        // integer
+        // The payment state of the subscription. Possible values are: 0. Payment pending 1. Payment received 2. Free trial 3. Pending deferred upgrade/downgrade
+        // Not present for canceled, expired subscriptions.
+        var paymentState: Int?
+
+        // cancelReason
+        // integer
+        // The reason why a subscription was canceled or is not auto-renewing. Possible values are: 0. User canceled the subscription 1. Subscription was canceled by the system, for example because of a billing problem 2. Subscription was replaced with a new subscription 3. Subscription was canceled by the developer
+        var cancelReason: Int?
+
+        // userCancellationTimeMillis
+        // string (int64 format)
+        // The time at which the subscription was canceled by the user, in milliseconds since the epoch. Only present if cancelReason is 0.
+        var userCancellationTimeMillis: Int64?
+
+        // cancelSurveyResult
+        // object (SubscriptionCancelSurveyResult)
+        // Information provided by the user when they complete the subscription cancellation flow (cancellation reason survey).
+
+        // orderId
+        // string
+        // The order id of the latest recurring order associated with the purchase of the subscription. If the subscription was canceled because payment was declined, this will be the order id from the payment declined order.
+        var orderId: String
+
+        // linkedPurchaseToken
+        // string
+        // The purchase token of the originating purchase if this subscription is one of the following: 0. Re-signup of a canceled but non-lapsed subscription 1. Upgrade/downgrade from a previous subscription
+        // For example, suppose a user originally signs up and you receive purchase token X, then the user cancels and goes through the resignup flow (before their subscription lapses) and you receive purchase token Y, and finally the user upgrades their subscription and you receive purchase token Z. If you call this API with purchase token Z, this field will be set to Y. If you call this API with purchase token Y, this field will be set to X. If you call this API with purchase token X, this field will not be set.
+        var linkedPurchaseToken: String?
+
+        // purchaseType
+        // integer
+        // The type of purchase of the subscription. This field is only set if this purchase was not made using the standard in-app billing flow. Possible values are: 0. Test (i.e. purchased from a license testing account) 1. Promo (i.e. purchased using a promo code)
+        var purchaseType: Int?
+
+        // priceChange
+        // object (SubscriptionPriceChange)
+        // The latest price change information available. This is present only when there is an upcoming price change for the subscription yet to be applied.
+        // Once the subscription renews with the new price or the subscription is canceled, no price change information will be returned.
+
+        // profileName
+        // string
+        // The profile name of the user when the subscription was purchased. Only present for purchases made with 'Subscribe with Google'.
+
+        // emailAddress
+        // string
+        // The email address of the user when the subscription was purchased. Only present for purchases made with 'Subscribe with Google'.
+
+        // givenName
+        // string
+        // The given name of the user when the subscription was purchased. Only present for purchases made with 'Subscribe with Google'.
+
+        // familyName
+        // string
+        // The family name of the user when the subscription was purchased. Only present for purchases made with 'Subscribe with Google'.
+
+        // profileId
+        // string
+        // The Google profile id of the user when the subscription was purchased. Only present for purchases made with 'Subscribe with Google'.
+
+        // acknowledgementState
+        // integer
+        // The acknowledgement state of the subscription product. Possible values are: 0. Yet to be acknowledged 1. Acknowledged
+        var acknowledgementState: Int
+
+        // externalAccountId
+        // string
+        // User account identifier in the third-party service. Only present if account linking happened as part of the subscription purchase flow.
+
+        // promotionType
+        // integer
+        // The type of promotion applied on this purchase. This field is only set if a promotion is applied when the subscription was purchased. Possible values are: 0. One time code 1. Vanity code
+        var promotionType: Int?
+
+        // promotionCode
+        // string
+        // The promotion code applied on this purchase. This field is only set if a vanity code promotion is applied when the subscription was purchased.
+        var promotionCode: String?
+
+        // obfuscatedExternalAccountId
+        // string
+        // An obfuscated version of the id that is uniquely associated with the user's account in your app. Present for the following purchases: * If account linking happened as part of the subscription purchase flow. * It was specified using https://developer.android.com/reference/com/android/billingclient/api/BillingFlowParams.Builder#setobfuscatedaccountid when the purchase was made.
+
+        // obfuscatedExternalProfileId
+        // string
+        // An obfuscated version of the id that is uniquely associated with the user's profile in your app. Only present if specified using https://developer.android.com/reference/com/android/billingclient/api/BillingFlowParams.Builder#setobfuscatedprofileid when the purchase was made.
+    }
+    
+}
