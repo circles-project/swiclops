c785763c4fd638fd1eb30faaaf948d46e2aff417
Charles Wright <cvwright@futo.org>
2022-10-14 09:08:18 -0500

    Change the order in which we delete things.  First deactivate with the server, then remove our own stuff locally.

--- Sources/App/Controllers/Endpoint Handlers/AccountDeactivateHandler.swift
+++ Sources/App/Controllers/Endpoint Handlers/AccountDeactivateHandler.swift
@@ -25,14 +25,18 @@ struct AccountDeactivateHandler: EndpointHandler {
             throw MatrixError(status: .unauthorized, errcode: .unauthorized, error: "This endpoint requires authentication")
         }
         
-        // 1. Deactivate the user in all of our authentication modules
-        for checker in checkers {
-            req.logger.debug("Unenrolling from auth checker for \(checker.getSupportedAuthTypes())")
-            try await checker.onUnenrolled(req: req, userId: user.userId)
+        // 1. Proxy the request on to the real homeserver so the account can be deactivated there as well
+        let response = try await proxy.handle(req: req)
+        
+        // 2. If we succesfully deactivated the account with the homeserver, only then do we deactivate the user in all of our authentication modules
+        if response.status == .ok {
+            for checker in checkers {
+                req.logger.debug("Unenrolling from auth checker for \(checker.getSupportedAuthTypes())")
+                try await checker.onUnenrolled(req: req, userId: user.userId)
+            }
         }
         
-        // 2. Proxy the request on to the real homeserver so the account can be deactivated there as well
-        return try await proxy.handle(req: req)
+        return response
     }
     
 }
