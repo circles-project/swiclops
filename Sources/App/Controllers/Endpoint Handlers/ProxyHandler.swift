4fca911b660a4c64b02eecb6fa5bcf19c35baaea
Charles Wright <cvwright@futo.org>
2022-10-14 09:24:20 -0500

    Debugging why /account/deactivate isn't working

--- Sources/App/Controllers/Endpoint Handlers/ProxyHandler.swift
+++ Sources/App/Controllers/Endpoint Handlers/ProxyHandler.swift
@@ -28,18 +28,20 @@ struct ProxyHandler: EndpointHandler {
     }
     
     func handle(req: Request) async throws -> Response {
-        var requestBody = try? req.content.decode(GenericContent.self)
-        guard requestBody != nil else {
+        //var requestBody = try? req.content.decode(GenericContent.self)
+        //guard requestBody != nil else {
+        guard let requestBody = try? req.content.decode(GenericContent.self) else {
             throw MatrixError(status: .badRequest, errcode: .badJson, error: "Couldn't parse request")
         }
         // Remove the "auth" object that was used for UIA
-        requestBody!["auth"] = nil
+        var myRequestBody = requestBody
+        myRequestBody["auth"] = nil
         
         // Now pass the rest of the request body on to the real homeserver
         let homeserverURI = URI(scheme: homeserver.scheme, host: homeserver.host, port: homeserver.port, path: req.url.path)
         req.logger.debug("ProxyHandler: Forwarding request to [\(homeserverURI)]")
 
-        let proxyResponse1 = try await req.client.post(homeserverURI, headers: req.headers, content: requestBody!)
+        let proxyResponse1 = try await req.client.post(homeserverURI, headers: req.headers, content: myRequestBody)
         
         
         if proxyResponse1.status == .unauthorized {
@@ -56,6 +58,7 @@ struct ProxyHandler: EndpointHandler {
             guard let responseBody = try? proxyResponse1.content.decode(GenericUiaResponse.self) else {
                 let msg = "Homeserver returned invalid UIA response"
                 req.logger.error("ProxyHandler: \(msg)")
+                req.logger.error("              Got response: \(proxyResponse1)")
                 throw MatrixError(status: .internalServerError, errcode: .unknown, error: msg)
             }
             let sessionId = responseBody.auth.session
@@ -63,9 +66,9 @@ struct ProxyHandler: EndpointHandler {
             let token = try SharedSecretAuth.token(secret: backendAuthConfig.sharedSecret, userId: userId)
             
             // Now we can send the authenticated version of the request
-            requestBody!["auth"] = AnyCodable(SharedSecretAuth.AuthDict(token: token, session: sessionId))
+            myRequestBody["auth"] = AnyCodable(SharedSecretAuth.AuthDict(token: token, session: sessionId))
             // And send the response back to the client
-            let authedResponse = try await req.client.post(homeserverURI, headers: req.headers, content: requestBody!)
+            let authedResponse = try await req.client.post(homeserverURI, headers: req.headers, content: myRequestBody)
             req.logger.debug("ProxyHandler: Got authed response with status \(authedResponse.status)")
             let authedResponseBody = Response.Body(buffer: authedResponse.body ?? .init())
             return Response(status: authedResponse.status, headers: authedResponse.headers, body: authedResponseBody)
