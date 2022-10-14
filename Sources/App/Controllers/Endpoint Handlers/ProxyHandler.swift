69b5ce00b064ac62b5572330c46240dc497f7b71
Charles Wright <cvwright@futo.org>
2022-10-14 14:45:45 -0500

    Now trying cRaZy stuff to try to make Synapse happy.  Putting an `identifier` in the auth dict...

--- Sources/App/Controllers/Endpoint Handlers/ProxyHandler.swift
+++ Sources/App/Controllers/Endpoint Handlers/ProxyHandler.swift
@@ -71,14 +71,14 @@ struct ProxyHandler: EndpointHandler {
             req.logger.debug("ProxyHandler: Computed token [\(token)] for user [\(userId)]")
             
             // Now we can send the authenticated version of the request
-            myRequestBody["auth"] = AnyCodable(SharedSecretAuth.AuthDict(token: token, session: sessionId))
-            // OK this is *weird* but Synapse appears to be demanding that we provide an `identifier` structure for UIA, like we do for `/login`
-            myRequestBody["identifier"] = AnyCodable(
-                [
-                    "type": "m.id.user",
-                    "user": userId
-                ]
-            )
+            myRequestBody["auth"] = AnyCodable(SharedSecretAuth.AuthDict(token: token,
+                                                                         session: sessionId,
+                                                                         identifier: [
+                                                                            "type": "m.id.user",
+                                                                            "user": userId,
+                                                                         ]
+                                                                        ))
+
             // Debugging: Did we craft a reasonable thing here as our request???
             if true {
                 let encoder = JSONEncoder()
