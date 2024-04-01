0a930a70b3d4c148db62848c9c6175d4e72c2ecd
Charles Wright <cvwright@futo.org>
2024-04-01 14:18:51 -0500

    Require MacOS v13 to get some features available on Linux but not on MacOS v12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swiclops",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
